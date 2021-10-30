a8f3f017cdc921ecf7106ae6ccd31db64b113b40
ltvinh9899 <37173634+ltvinh9899@users.noreply.github.com>
2021-10-30 10:31:20 +0700

    firt commit

---
+++ UETshare/src/main/java/java/spring/uetshare/UetshareApplication.java
@@ -0,0 +1,13 @@
+package java.spring.uetshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UetshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UetshareApplication.class, args);
+	}
+
+}
---
+++ UETshare/src/test/java/java/spring/uetshare/UetshareApplicationTests.java
@@ -0,0 +1,13 @@
+package java.spring.uetshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UetshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
